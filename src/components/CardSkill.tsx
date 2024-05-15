c16dc9a0fc7531ae7f69d56e812410d49d76ce49
mandrindra <mandrindra.ranarijaona@devngo.fr>
2024-05-15 19:44:36 +0300

    update card skill

--- src/components/CardSkill.tsx
+++ src/components/CardSkill.tsx
@@ -6,13 +6,13 @@ import "assets/scss/cardSkill.scss";
 
 const CardSkill: React.FC<Skill> = ({ name, icon, rating, id }) => {
   return (
-    <div className="card-skill-container">
+    <div className="card-skill-container ">
       <h5 className="card-name">{name}</h5>
 
       <p className="card-icon">{icon()}</p>
-      <div className="card-rating ">
+     <div className="card-rating ">
         <Rating rating={rating} id={id} />
-      </div>
+      </div> 
     </div>
   );
 };
